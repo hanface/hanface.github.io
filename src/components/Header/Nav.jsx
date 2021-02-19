86893444ea8c90e032966364df74456ce97c9368
hanface <contacthanahmae@gmail.com>
2021-02-19 13:38:48 +1300

    added current resume

--- src/components/Header/Nav.jsx
+++ src/components/Header/Nav.jsx
@@ -8,6 +8,13 @@ function Nav() {
       <AnchorLink href="#about">About</AnchorLink>
       <AnchorLink href="#work">Work</AnchorLink>
       <AnchorLink href="#contact">Contact</AnchorLink>
+      <a
+        class="button resume-button"
+        href="/han-thompson-resume2021.pdf"
+        target="_blank"
+      >
+        Resume
+      </a>
     </nav>
   );
 }
